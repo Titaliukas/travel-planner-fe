8305fdeda54768c3ab80a5b3034544ec7df8790b
rokzia <rokasz981@gmail.com>
2026-04-06 18:20:27 +0300

    .net backend project carcass

---
+++ backend/Program.cs
@@ -0,0 +1,24 @@
+
+namespace backend
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
